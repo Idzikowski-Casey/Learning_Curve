8e0e4a8280bb3998fc88a5810a5495cc7c9ca99e
Casey Idzikowski <idzikoj1234@gmail.com>
2020-05-16 13:28:24 -0500

    Initial Commit

---
+++ Calc.py
@@ -0,0 +1,21 @@
+from Numbers import *
+
+def add(a,b):
+    return a + b
+
+def div(a,b):
+    return a / b
+
+def sub(a,b):
+    return a - b
+
+def mult(a,b):
+    return a*b
+
+def sqred(a):
+    return a*a
+
+D = sqred(random)
+
+H = sub(random2, random)
+
---
+++ Numbers.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+random = np.random.randint(1, 49, 6)
+
+random2 = np.random.randint(50, 100, 6)
+
+random3 = np.random.randint(1, 100, 6)
+
